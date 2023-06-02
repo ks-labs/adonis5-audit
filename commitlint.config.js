24e58e5219afa621a7be50a1c112d4891b636ac0
Vinicios Clarindo <vinicioslc@outlook.com>
2023-06-02 18:11:54 -0300

    :pencil: (commits) add new commit and changelog generation for `cm` and `cma` command

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,13 @@
+const czEmoji = require('./.cz-emoji')
+
+const allowedTypes = czEmoji.types?.map((value) => {
+  return value.name
+})
+
 module.exports = {
-  extends: ['gitmoji'],
+  extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [0, 'always', 100],
+    'type-enum': [2, 'always', allowedTypes],
   },
 }
